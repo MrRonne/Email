a86cddc36ffa9ed10492b6d068c3a045291128c8
MrRonne <50634699+MrRonne@users.noreply.github.com>
2019-10-29 13:06:39 +0500

    Fixed checking

--- Email/Program.cs
+++ Email/Program.cs
@@ -12,7 +12,7 @@ namespace Email
             builder.SetTitle("3 task");
             var sender = builder.GetSender("usu.courses@gmail.com");
             sender.AddRecipient("mr.ronne@yandex.ru");
-            if (sender.GetRecipients().Count() == sender.GetMails().Count())
+            if (sender.GetRecipients().Count() != sender.GetMails().Count())
                 throw new Exception("Something went wrong");
             sender.SendMails();
         }
